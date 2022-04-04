f3bbca836c4b132fe6f4bb73e4f57dd9aa72834a
Glei <90923556+Glei-H@users.noreply.github.com>
2022-04-04 17:48:50 -0500

    Completed Form

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from crypt import methods
+import string
 from flask import Flask, redirect, request, url_for
 from flask import render_template
 
@@ -12,22 +13,21 @@ def index():
 def estimate():
     if request.method == 'POST':
         form = request.form
-        radius = form['radius']
-        height = form['height']
-        top_area = 3.14 * radius^2
-        side_area = 2(3.14(radius*height))
+        radius = float(form['radius'])
+        height = float(form['height'])
+        top_area = 3.14 * radius**2
+        side_area = 2*3.14*radius*height
         total_area = top_area + side_area
         total_sqft = total_area / 144
-        material_cost = 25
-        total_material_cost = total_sqft * material_cost
-        labor_cost = 15
-        total_labor_cost =  total_sqft * labor_cost
+        total_material_cost = total_sqft * 25
+        total_labor_cost =  total_sqft * 15
         total_cost_estimate = total_labor_cost + total_material_cost
-        #fname = form['radius']
-        #lname = form['diameter']
+        estimate = round(total_cost_estimate, 2)
+        return render_template('estimate.html', quote=estimate)
+    return render_template('estimate.html', pageTitle= 'Estimate VPM')
+     #print(total_cost_estimate)
         #return redirect(url_for('index'))
     #return redirect(url_for('index'))
-    return render_template('estimate.html', pageTitle= 'Estimate VPM')
 
 if __name__ == '__main__':
     app.run(debug=True) #set to false when putting on azure
