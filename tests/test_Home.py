acbd4e1b7fbafc7d14bd6a3360bb57483a8262b5
Glei <90923556+Glei-H@users.noreply.github.com>
2022-04-26 00:17:49 -0500

    Update test_Home.py

--- tests/test_Home.py
+++ tests/test_Home.py
@@ -1,3 +1,5 @@
+import pytest
+ 
 def test_inputs(app, client):
    with app.test_client() as test_client:
        res = test_client.get('/')
@@ -12,5 +14,14 @@ def test_about(app, client):
 def test_estimate(app, client):
    with app.test_client() as test_client:
        res = test_client.get('/estimate')
-       assert res.status_code == 200)
+       assert res.status_code == 200
+
+def test_functionality(app, client):
+    print(" -- /result 'estimate_total' POST test")
+    with app.test_client() as test_client:
+        cost = {"radius": "180", "height": "360"}
+        res = test_client.post('/estimate', data=cost)
+        assert res.status_code == 200
+        assert b"141,300"
+
        
